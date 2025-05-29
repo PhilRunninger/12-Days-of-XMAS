6f9a15edeb360241c0e16fd35796fe06148d469f
2pNnrF <2pNnrF@cyber-dojo.org>
2025-05-29 11:49:32 +0000

    20 ran tests, no prediction, got red

--- files/TwelveDays.java
+++ files/TwelveDays.java
@@ -13,7 +13,7 @@ public class TwelveDays
             if (i == 1 && day > 1) {
                 result += " and";
             }
-            result += loot[day-1];
+            result += String.format(" %s", loot[day-1]);
         }
         return result;
     }
