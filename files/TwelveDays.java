c4cf7b06ae3d7cedd66c1dbb21f994a7b56aa00b
2pNnrF <2pNnrF@cyber-dojo.org>
2025-05-29 11:48:32 +0000

    19 ran tests, no prediction, got red

--- files/TwelveDays.java
+++ files/TwelveDays.java
@@ -1,7 +1,20 @@
+import java.util.HashMap;
+
 public class TwelveDays
 {
     public static String sing(int day)
     {
-        return "On the first day of Christmas My true love gave to me: A partridge in a pear tree.";
+        String[] days = {"first", "second"};
+        String[] loot = {"A partridge in a pear tree", "Two turtle doves"};
+
+        String result = String.format("On the %s day of Christmas My true love gave to me:", days[day-1]);
+        for (int i=day; i>0; i--)
+        {
+            if (i == 1 && day > 1) {
+                result += " and";
+            }
+            result += loot[day-1];
+        }
+        return result;
     }
 }
