f0c50849d1f0f80cef589a67845eae02367e2107
2pNnrF <2pNnrF@cyber-dojo.org>
2025-05-29 05:11:35 +0000

    17 ran tests, no prediction, got green

--- files/TwelveDays.java
+++ files/TwelveDays.java
@@ -2,6 +2,6 @@ public class TwelveDays
 {
     public static String sing(int day)
     {
-        return "On the first day of Christmas\nMy true love gave to me:\nA partridge in a pear tree.";
+        return "On the first day of Christmas My true love gave to me: A partridge in a pear tree.";
     }
 }
