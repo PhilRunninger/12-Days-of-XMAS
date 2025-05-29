0dc28e2afa3827946ba9c6eb5edad7e1454273c8
2pNnrF <2pNnrF@cyber-dojo.org>
2025-05-29 04:43:34 +0000

    6 ran tests, no prediction, got amber

--- files/TwelveDays.java
+++ files/TwelveDays.java
@@ -2,6 +2,6 @@ class TwelveDays
 {
     String sing(int day)
     {
-        return "On the first day of Christmas\nMy true love gave to me:\nA partridge in a pear tree."
+        return "On the first day of Christmas\nMy true love gave to me:\nA partridge in a pear tree.";
     }
 }
